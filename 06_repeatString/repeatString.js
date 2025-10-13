17c3ce5fe3711c2e6fd66132b9c45cead0c37a04
Sumit <73896616+sumitpaul7june@users.noreply.github.com>
2025-10-13 21:01:04 +0530

    Completed 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+
+    if( n < 0) return "ERROR";
+    let value = "";
+    for(let i = 0; i < n; i++)
+    {
+        value += str;
+    }
+    return value;
 
 };
 
