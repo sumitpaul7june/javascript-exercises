b9d124af05eeaab4e4f6b682fea93d86503d9d01
Sumit <73896616+sumitpaul7june@users.noreply.github.com>
2025-10-13 21:22:55 +0530

    Completed 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(str) {
+
+    let reversedStr = "";
+    for(let i = str.length - 1; i >= 0; i--)
+    {
+        reversedStr = reversedStr + str.charAt(i);
+    }
+    return reversedStr;
 
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
