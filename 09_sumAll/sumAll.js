241c755a0cad58b0923cce976fd586258f9a1743
Sumit <73896616+sumitpaul7june@users.noreply.github.com>
2025-10-13 22:33:19 +0530

    Completed 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+
+    if(a < 0 || b < 0) return "ERROR";
+    
+    let max = a > b ? a : b;
+    let min = a < b ? a : b;
+
+    if(min === Math.floor(min) && max === Math.floor(max))
+    {
+        let sum = 0;
+        for(let i = min; i <= max; i++)
+        {
+            sum += i;
+        }
+
+        return sum;
+    }
+    else
+    {
+        return "ERROR";
+    }
+    
 
 };
 
