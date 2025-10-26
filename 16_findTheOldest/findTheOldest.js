b6b5c5f4b85b0411e7d85701eee117e3a692c236
Sumit <73896616+sumitpaul7june@users.noreply.github.com>
2025-10-26 18:45:08 +0530

    Solution for 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(persons) {
 
+    const date = new Date();
+    return persons.reduce(function(oldest, obj)
+{
+
+    let age = (obj.yearOfDeath || date.getFullYear()) - obj.yearOfBirth;
+    let oldestAge = (oldest.yearOfDeath || date.getFullYear()) - oldest.yearOfBirth;
+
+   if (age > oldestAge) return obj;
+   else return oldest;
+
+})
 };
 
 // Do not edit below this line
