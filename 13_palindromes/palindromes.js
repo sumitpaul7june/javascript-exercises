baed1b3a98b0b7c152e248f382d78afa77fc60f4
Sumit <73896616+sumitpaul7june@users.noreply.github.com>
2025-10-26 17:34:14 +0530

    Solution for 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,37 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+
+    let lowerCaseString = string.toLowerCase();
+
+    let lowerCaseStringWithAlphabetsAndDigitsOnly = '';
+
+    for (let i = 0; i < lowerCaseString.length; i++) {
+        if ((lowerCaseString[i] >= 'a' && lowerCaseString[i] <= 'z') || (lowerCaseString[i] >= '0' && lowerCaseString[i] <= '9')) {
+            lowerCaseStringWithAlphabetsAndDigitsOnly += lowerCaseString[i];
+        }
+    }
+
+    console.log(lowerCaseStringWithAlphabetsAndDigitsOnly);
+    
+    let i = 0, j = lowerCaseStringWithAlphabetsAndDigitsOnly.length - 1;
+
+    while (i < j) {
+        if (lowerCaseStringWithAlphabetsAndDigitsOnly[i] !== lowerCaseStringWithAlphabetsAndDigitsOnly[j]) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+
+    return true;
+
+
+
 
 };
+
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
