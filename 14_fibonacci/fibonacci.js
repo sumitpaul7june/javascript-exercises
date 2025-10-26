2f7c6904232aa34c8caab91ac71432fc47e79ca8
Sumit <73896616+sumitpaul7june@users.noreply.github.com>
2025-10-26 17:55:06 +0530

    Solution for 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+
+    num = parseInt(num)
+    if(num < 0) return 'OOPS';
+    if(num === 0) return 0;
+    let prev = 1, next = 1;
+
+    if(num === 1  || num === 2) return prev;
+    let value = 0;
+    for(let i = 2; i < num; i++) 
+    {
+        value = prev + next;
+        prev = next;
+        next = value;
+    }
+    return value;
 
 };
 
